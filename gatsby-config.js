2ca8b6fa8f8bf05708e60dc5e1a18fdd88ee84d4
lenvin gonsalves <lenvingonsalves@gmail.com>
2020-06-26 00:37:09 +0530

    SEO changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
+    title: `Laveena Gonsalves`,
+    name: `Laveena Gonsalves`,
     siteUrl: `https://novela.narative.co`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Welcome to my website`,
       maxWidth: 652,
     },
     social: [
